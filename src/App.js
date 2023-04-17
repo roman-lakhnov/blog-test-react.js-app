029005a66154e191b2aef59b8c2413ccc0daeef4
Roman Lakhnov <lakhnov.roman@gmail.com>
2023-04-17 09:58:41 +0300

    UI btn and input added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { useState } from 'react'
 import Counter from './components/Counter.jsx'
 import ClassCounter from './components/ClassCounter.jsx'
+import './styles/App.css'
+import PostItem from './components/PostItem.jsx'
+import PostList from './components/PostList.jsx'
+import MyButton from './components/UI/button/MyButton.jsx'
+import MyInput from './components/UI/input/MyInput.jsx'
 
 function App() {
-  const [value,setValue]=useState('input text')
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript', body: 'Description' },
+		{ id: 2, title: 'JavaScript 2', body: 'Description' },
+		{ id: 3, title: 'JavaScript 3', body: 'Description' }
+	])
+	const [title, setTitle] = useState('')
+
+	function addNewPost(e) {
+		e.preventDefault()
+		console.log({ title })
+	}
 
 	return (
 		<div className='App'>
-		<Counter/>
-		<ClassCounter/>
+			<form action=''>
+				<MyInput
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+					type='text'
+					placeholder='Title'
+				/>
+				<MyInput type='text' placeholder='Body' />
+				<MyButton onClick={addNewPost}>Create</MyButton>
+			</form>
+
+			<PostList posts={posts} title='Posts List 1' />
 		</div>
 	)
 }
