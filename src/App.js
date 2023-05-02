df6f9248910d288755deab100d846d9d8bb50b6f
Roman Lakhnov <lakhnov.roman@gmail.com>
2023-05-02 10:39:03 +0300

    pagination added

--- src/App.js
+++ src/App.js
@@ -10,25 +10,37 @@ import { useEffect } from 'react'
 import PostService from './API/PostService'
 import Loader from './components/UI/Loader/Loader'
 import { useFetching } from './hooks/useFetching'
+import { getPageCount } from './utils/pages'
+import Pagination from './components/UI/pagination/Pagination'
 
 function App() {
 	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [modal, setModal] = useState(false)
+	const [totalPages, setTotalPages] = useState(0)
+	const [limit, setLimit] = useState(10)
+	const [page, setPage] = useState(1)
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-	const [fetchPosts,isPostsLoading,postError]=useFetching(async()=>{
-		const posts = await PostService.getAll()
-		setPosts(posts)
-	})
+
+	const [fetchPosts, isPostsLoading, postError] = useFetching(
+		async (limit, page) => {
+			const response = await PostService.getAll(limit, page)
+			setPosts(response.data)
+			const totalCount = response.headers['x-total-count']
+			setTotalPages(getPageCount(totalCount, limit))
+		}
+	)
 
 	// useEffect(() => {
 	// 	document.title = `Posts ${sortedAndSearchedPosts.length}`
 	// })
 
+	console.log(totalPages)
+
 	useEffect(() => {
-		fetchPosts()
+		fetchPosts(limit, page)
 		return () => {}
-	},[])
+	}, [])
 
 	const createPost = newPost => {
 		setPosts([...posts, newPost])
@@ -37,6 +49,12 @@ function App() {
 	const removePost = post => {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
+
+	const changePage = page => {
+		setPage(page)
+		fetchPosts(limit, page)
+	}
+
 	return (
 		<div className='App'>
 			<MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
@@ -62,9 +80,10 @@ function App() {
 				<PostList
 					remove={removePost}
 					posts={sortedAndSearchedPosts}
-					title={sortedAndSearchedPosts.length?'Posts List':'No post found'} 
+					title={sortedAndSearchedPosts.length ? 'Posts List' : 'No post found'}
 				/>
 			)}
+			<Pagination page={page} totalPages={totalPages} changePage={changePage} />
 		</div>
 	)
 }
