0dcbbc4e6691037859eaab244692b097b51b9eb5
Roman Lakhnov <lakhnov.roman@gmail.com>
2023-05-05 11:55:44 +0300

    routes arr added

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,10 +4,19 @@ import About from '../pages/About'
 import Error from '../pages/Error'
 import Posts from '../pages/Posts'
 import PostIdPage from '../pages/PostIdPage'
+import { routes } from '../router/routes'
 
 function AppRouter() {
 	return (
 		<Routes>
+			{routes.map(route => 
+				<Route
+					exact={route.exact}
+					path={route.path}
+					element={route.component}
+				/>
+			)}
+
 			<Route path='/about' element={<About />} />
 			<Route exact path='/posts' element={<Posts />} />
 			<Route exact path='/posts/:id' element={<PostIdPage />} />
